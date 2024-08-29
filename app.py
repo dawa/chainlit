9459018a2c01d947861790b518fd97a8f62c6bfe
Davis Wamola <dawa009@yahoo.com>
2024-08-28 21:32:33 -0700

    Switch back to gtp4o vision model

--- app.py
+++ app.py
@@ -3,28 +3,28 @@ import openai
 import os
 import base64
 
-# api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 
-api_key = os.getenv("RUNPOD_API_KEY")
-runpod_serverless_id = os.getenv("RUNPOD_SERVERLESS_ID")
+# api_key = os.getenv("RUNPOD_API_KEY")
+# runpod_serverless_id = os.getenv("RUNPOD_SERVERLESS_ID")
 
-# endpoint_url = "https://api.openai.com/v1"
-endpoint_url = f"https://api.runpod.ai/v2/{runpod_serverless_id}/openai/v1"
+endpoint_url = "https://api.openai.com/v1"
+# endpoint_url = f"https://api.runpod.ai/v2/{runpod_serverless_id}/openai/v1"
 
 client = openai.AsyncClient(api_key=api_key, base_url=endpoint_url)
 
 # https://platform.openai.com/docs/models/gpt-4o
-# model_kwargs = {
-#     "model": "chatgpt-4o-latest",
-#     "temperature": 0.3,
-#     "max_tokens": 1000
-# }
-
 model_kwargs = {
-    "model": "mistralai/Mistral-7B-Instruct-v0.3",
+    "model": "chatgpt-4o-latest",
     "temperature": 0.3,
     "max_tokens": 1000
 }
+
+# model_kwargs = {
+#     "model": "mistralai/Mistral-7B-Instruct-v0.3",
+#     "temperature": 0.3,
+#     "max_tokens": 1000
+# }
 
 @cl.on_message
 async def on_message(message: cl.Message):
